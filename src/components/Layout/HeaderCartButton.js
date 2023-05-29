86057875cd2690819f4ba24e6909fc0d75ef7459
Serwat Ali <serwatali@gmail.com>
2023-05-29 21:10:47 +0530

    add use context

--- src/components/Layout/HeaderCartButton.js
+++ src/components/Layout/HeaderCartButton.js
@@ -1,17 +1,43 @@
-import React from "react";
+import React, { useContext, useEffect, useState } from "react";
 
 import CartIcon from "../Cart/CartIcon";
+import CartContext from "../../store/cart-context";
 import styles from "./HeaderCartButton.module.css";
 
 const HeaderCartButton = (props) => {
+  const [btnIsHighlighted, setBtnIsHighlighted] = useState(false);
+
+  const cartCtx = useContext(CartContext);
+
+  const { items } = cartCtx;
+
+  const numberOfCartItems = items.reduce((curNumber, item) => {
+    return curNumber + item.amount;
+  }, 0);
+
+  const btnStyles = `${styles.button} ${btnIsHighlighted ? styles.bump : ""}`;
+  useEffect(() => {
+    if (items.length === 0) {
+      return;
+    }
+    setBtnIsHighlighted(true);
+    const timer = setTimeout(() => {
+      setBtnIsHighlighted(false);
+    }, 300);
+
+    return () => {
+      clearTimeout(timer);
+    };
+  }, [items]);
+
   return (
     <>
-      <button className={styles.button}>
+      <button className={btnStyles} onClick={props.onClick}>
         <span className={styles.icon}>
           <CartIcon />
         </span>
         <span> Your Cart</span>
-        <span className={styles.badge}>3</span>
+        <span className={styles.badge}>{numberOfCartItems}</span>
       </button>
     </>
   );
